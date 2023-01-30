527d53bf439637d32402f8df8a077c3798385f67
bt-7274-jack <ashwin.kumar2@flipkart.com>
2023-01-30 16:49:28 +0530

    added the behaviour for students

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/StudentService.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/StudentService.java
@@ -9,4 +9,44 @@ package com.flipkart.service;
  */
 public class StudentService {
 	
+	
+	
+	public void courseList() {
+		// get the list of all the courses and return it.
+		
+	}
+	
+	public void registerCourse() {
+		// register for the courses
+		
+	}
+	
+	public void approvedList() {
+		// add/drop for the courses
+		
+	}
+	
+	public void getTotalFee() {
+		// calculate the total fees based on the registered courses.
+		
+	}
+	
+	public void payFee() {
+		// pay for the fees and return receipt
+		/*
+		 * 
+		 * if else for the payment status.
+		 * 
+		 */
+		
+	}
+	
+	
+	
+	public void viewGrade() {
+		//view the grade card with exception handling
+	}
+	
+	
+	
 }
