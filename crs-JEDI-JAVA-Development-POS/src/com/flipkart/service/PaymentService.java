205effba25da0ab0bacd1693bfa7a0e0980157a3
bt-7274-jack <123713780+bt-7274-jack@users.noreply.github.com>
2023-01-31 10:49:41 +0530

    Create PaymentService.java

---
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/PaymentService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.flipkart.service;
+
+/**
+ * @author ashwin.kumar2
+ *
+ */
+public class PaymentService {
+	
+	public void payOnline() {
+		// payment for the online mode
+		
+	}
+	
+	public void payOffline() {
+		// payment for the offline mode
+		
+	}
+
+}
