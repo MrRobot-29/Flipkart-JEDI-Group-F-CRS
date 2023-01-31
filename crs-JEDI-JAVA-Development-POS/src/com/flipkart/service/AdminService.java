386ff843bfa26808dd02a17a54c11a931ba2b6fd
rajmittal2209 <rajmittal2209@gmail.com>
2023-01-31 10:00:07 +0530

    adminservice

---
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/AdminService.java
@@ -0,0 +1,60 @@
+
+package com.flipkart.service;
+
+
+public class AdminService(){
+
+    // course services
+    public void dropCourse(String dropCourseCode) {
+
+    }
+
+    public void addCourse(Course newCourse) {
+
+    }
+
+    public void assignCourse(String courseCode, String professorId){
+
+    }
+
+    public List<Course> viewCourses(int catalogId) {
+
+    }
+
+// student related services
+
+    public List<Student> viewPendingStudents() {
+
+    }
+
+    public List<Student> viewAllStudents() {
+
+    }
+
+    public void validateStudent(int studentId){
+
+
+    }
+
+
+// professor related services
+
+    public void addProfessor(Professor professor) {
+
+    }
+
+    public void dropProfessor(Professor professor){
+
+    }
+    public List<Professor> viewProfessors() {
+
+    }
+
+
+    // gradecard related services
+    public void generateGradeCard(String studentId, String semester){
+
+    }
+}
+
+
