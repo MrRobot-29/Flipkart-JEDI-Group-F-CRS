c1709c77e45d0e5cf7fa63b6d8cafcede99dfb71
MrRobot-29 <gaurav.dash@flipkart.com>
2023-01-31 10:32:32 +0530

    Added admin bean

---
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Admin.java
@@ -0,0 +1,27 @@
+package com.flipkart.bean;
+
+import java.util.Date;
+
+public class Admin extends User{
+	
+	
+	public void approveStudent() {
+		
+	}
+	
+
+	public void addProfessor() {
+		
+	}
+	
+	public void addAdmin() {
+		
+	}
+	
+	public void generateReportCard() {
+		
+	}
+	public void modifyCourseCatlogue() {
+		
+	}
+}
