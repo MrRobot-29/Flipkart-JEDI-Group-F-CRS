55fd05932ab00bdb83516b5bbc06cf0634e69415
kanhaiya singhal <kanhaiya.singhal@flipkart.com>
2023-01-31 15:22:21 +0530

    changes in professor crs menu

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/client/ProfessorCRSMenu.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/client/ProfessorCRSMenu.java
@@ -1,5 +1,45 @@
 package com.flipkart.client;
 
+import java.util.Scanner;
+
 public class ProfessorCRSMenu {
+	
+	
+	
+	public void createMenu() {
+		boolean isLogin = true;
+		while(isLogin) {
+			System.out.print("Enter your choise : ");
+			System.out.println("1. viewCourse");
+			System.out.println("2. View Enrolled Student");
+			System.out.println("3. Add Grade");
+			System.out.println("4. log out");
+			
+			Scanner sc = new Scanner(System.in);
+			
+			int selectedOption = sc.nextInt();
+			
+			boolean isExit = false;
+			while(!isExit)
+			{
+				switch(selectedOption)
+				{
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					isExit = true;
+					break;
+				default:
+					break;
+						
+				}
+			}
+		}
+		
+	}
 
 }
