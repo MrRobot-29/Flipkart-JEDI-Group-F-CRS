8547f1a0832e7356e96626fe2241eb76914cef37
MrRobot-29 <gaurav.dash@flipkart.com>
2023-01-31 09:45:14 +0530

    Added Student class in bean

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
@@ -8,6 +8,30 @@ package com.flipkart.bean;
  *
  */
 public class Student {
+	private int studentID;
+	private String branch;
 	
-
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	
+	public void viewGrade() {
+		System.out.println("Student Grade");
+	}
+	public void register() {
+		System.out.println("Register student for courses");
+	}
+	public void payFee() {
+		System.out.println("Student pay fee");
+	}
+	
 }
