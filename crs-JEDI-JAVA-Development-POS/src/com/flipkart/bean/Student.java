88303844a5ff51ef668455efaa13d9038cb0e5f2
MrRobot-29 <gaurav.dash@flipkart.com>
2023-01-31 09:54:12 +0530

    Added student constructor params

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
@@ -2,14 +2,26 @@
  * 
  */
 package com.flipkart.bean;
+import com.flipkart.constant.Gender;
+import com.flipkart.constant.Role;
 
 /**
  * @author gaurav.dash
  *
  */
-public class Student {
+public class Student extends User {
 	private int studentID;
 	private String branch;
+	private boolean isApproved;
+	
+
+	public Student(String userId, String name, Role role, String password, Gender gender, String address,
+			String country,String branchName,int studentId,int batch,boolean isApproved) {
+		super(userId, name, role, password, gender, address, country);
+		this.branch = branchName;
+		this.studentID = studentId;
+		this.isApproved = isApproved;
+	}
 	
 	public int getStudentID() {
 		return studentID;
@@ -23,7 +35,15 @@ public class Student {
 	public void setBranch(String branch) {
 		this.branch = branch;
 	}
+	public boolean isApproved() {
+		return isApproved;
+	}
+
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
 	
+	// methods start
 	public void viewGrade() {
 		System.out.println("Student Grade");
 	}
