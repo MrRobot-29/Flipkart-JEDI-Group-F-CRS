6dc6f08a1d02559887a2f39d9d99c708aa9fce16
Shivamflipster <shivam.agrawal1@flipkart.com>
2023-02-02 14:47:27 +0530

    Added admin login and multiple action feature

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/client/AdminCRSMenu.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/client/AdminCRSMenu.java
@@ -8,67 +8,71 @@ import com.flipkart.service.AdminServiceOperation;
 public class AdminCRSMenu {
 	Scanner scanner = new Scanner(System.in); 
 	AdminServiceOperation aso = new AdminServiceOperation();
+	boolean isLoggedIn = true;
 	public void createMenu(String user) {
 		
-		
-		System.out.println("***********************************");
-		System.out.println("********* Admin *******************");
-		System.out.println("***********************************");
-		System.out.println("Login as : " + user + "\n");
-		System.out.println("1. View course in catalog");
-		System.out.println("2. Add Course to catalog");
-		System.out.println("3. Delete Course from catalog");
-		System.out.println("4. Approve Students");
-		System.out.println("5. Add Professor");
-		System.out.println("6. Logout");
-		System.out.println("*****************************");
-		
-		int choice = scanner.nextInt();
-		
-		switch(choice) {
-		case 1:
+		while(isLoggedIn) {
+			System.out.println("***********************************");
+			System.out.println("********* Admin *******************");
+			System.out.println("***********************************");
+			System.out.println("Login as : " + user + "\n");
+			System.out.println("1. View course in catalog");
+			System.out.println("2. Add Course to catalog");
+			System.out.println("3. Delete Course from catalog");
+			System.out.println("4. Approve Students");
+			System.out.println("5. Add Professor");
+			System.out.println("6. Logout");
+			System.out.println("*****************************");
+			
+			int choice = scanner.nextInt();
+			
+			switch(choice) {
+			case 1:
 
-			ArrayList<Course> cl = aso.viewCourses();
-			int num = 1;
-			for(var c: cl) {
-				System.out.println(num);
-				System.out.println("Course Name: " + c.getCourseName());
-				System.out.println("Course ID: " + c.getCourseId());
-				num++;
+				ArrayList<Course> cl = aso.viewCourses();
+				int num = 1;
+				for(var c: cl) {
+					System.out.println(num);
+					System.out.println("Course Name: " + c.getCourseName());
+					System.out.println("Course ID: " + c.getCourseId());
+					num++;
+				}
+
+				
+				System.out.println("viewCoursesInCatalogue()");
+				
+
+				break;
+				
+			case 2:
+				if(aso.addCourse()) {
+					System.out.println("Course Added to the catalog");	
+				}
+				break;
+				
+			case 3:
+				if(aso.dropCourse()) {
+				    System.out.println("Course Dropped Successfully from the catalog");	
+			    }
+				break;
+				
+			case 4:
+				System.out.println("approveStudent()");
+				break;
+			
+
+			
+			case 5:
+				System.out.println("addProfessor()");
+				break;
+			
+			case 6:
+				isLoggedIn = false;
+				break;
+			
+			default:
+				System.out.println("Wrong Option");
 			}
-
-			
-			System.out.println("viewCoursesInCatalogue()");
-			
-
-			break;
-			
-		case 2:
-			if(aso.addCourse()) {
-				System.out.println("Course Added to the catalog");	
-			}
-			break;
-			
-		case 3:
-			if(aso.dropCourse()) {
-			    System.out.println("Course Dropped Successfully from the catalog");	
-		    }
-			break;
-			
-		case 4:
-			System.out.println("approveStudent()");
-			break;
-		
-
-		
-		case 5:
-			System.out.println("addProfessor()");
-			break;
-		
-
-		
-		default:
-			System.out.println("Wrong Option");
 		}
 		
 	}
