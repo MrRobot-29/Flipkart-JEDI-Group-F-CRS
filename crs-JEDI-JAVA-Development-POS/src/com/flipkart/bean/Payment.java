d95f30950e25af31920cf9fac9ec01051555f3f7
makuflip <manish.kumar24@flipkart.com>
2023-02-06 20:48:35 +0530

    Added comments

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Payment.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Payment.java
@@ -1,41 +1,91 @@
 package com.flipkart.bean;
 
+/**
+ * class to store payment
+ */
 public class Payment {
 	public int studentID;
 	public String referenceID;
 	public float paymentAmount;
 	public String modeOfPayment;
 	public boolean status;
-	
-	
+
+	/**
+	 *
+	 * @return student id
+	 */
 	public int getStudentID() {
 		return studentID;
 	}
+
+	/**
+	 * Method to set student id
+	 * @param studentID: id of student
+	 */
 	public void setStudentID(int studentID) {
 		this.studentID = studentID;
 	}
+
+	/**
+	 *
+	 * @return reference id of payment
+	 */
 	public String getReferenceID() {
 		return referenceID;
 	}
+
+	/**
+	 * Method to set reference id
+	 * @param referenceID: reference id of payment
+	 */
 	public void setReferenceID(String referenceID) {
 		this.referenceID = referenceID;
 	}
+
+	/**
+	 *
+	 * @return payment amount
+	 */
 	public float getPaymentAmount() {
 		return paymentAmount;
 	}
+
+	/**
+	 *
+	 * @param paymentAmount: payment amount
+	 */
 	public void setPaymentAmount(float paymentAmount) {
 		this.paymentAmount = paymentAmount;
 	}
+
+	/**
+	 *
+	 * @return status of payment
+	 */
 	public boolean isStatus() {
 		return status;
 	}
+
+	/**
+	 * Method to set payment status
+	 * @param status: status of payment
+	 */
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-	
+
+	/**
+	 *
+	 * @return mode of payment
+	 */
 	public String getModeOfPayment() {
 		return modeOfPayment;
 	}
+
+	/**
+	 * Method to set mode of payment
+	 * @param modeOfPayment: mode of payment
+	 */
 	public void setModeOfPayment(String modeOfPayment) {
 		this.modeOfPayment = modeOfPayment;
 	}
--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/ProfessorService.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/service/ProfessorService.java
@@ -2,14 +2,41 @@ package com.flipkart.service;
 
 import java.util.List;
 
+/**
+ * interface for professor service operation
+ */
 public interface ProfessorService {
-	
+
+	/**
+	 *
+	 * @param instructorId: instructor id
+	 * @return returns list of courses the professor is teaching
+	 */
 	public List<String> viewCourseList(int instructorId);
-	
+
+	/**
+	 *
+	 * @param courseId: course id
+	 * @param instructorId: instructor id
+	 * @return status of assigning the course to professor for teaching
+	 */
 	public boolean selectCourseToTeach(String courseId, int instructorId);
-	
+
+	/**
+	 *
+	 * @param instructorId: instructor id
+	 * @param courseId: course id
+	 * @return list of enrolled students corresponding to current professor id and course id
+	 */
 	public List<String> viewEnrolledStudents(int instructorId,String courseId);
-	
+
+	/**
+	 *
+	 * @param courseId: course id
+	 * @param studentId: student id
+	 * @param grade: student grade
+	 * @return returns status if grade is added or not
+	 */
 	 public boolean addGrade(String courseId, int studentId, String grade);
 	
 }
