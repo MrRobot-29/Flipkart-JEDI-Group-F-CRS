95a187be9a0abb8e88a6d99eddb4a43d98fc3b39
Archit Dalania <archit.dalania@BLRETV-0XKXTJWQ.local>
2023-02-06 20:44:44 +0530

    Resolved merge issues

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/helper/DaoHelper.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/helper/DaoHelper.java
@@ -15,13 +15,8 @@ public class DaoHelper {
 	
 	private static Connection connection = null;
 	
-	
-	public static Connection getConnection() {
-		connection = connectToDB();
-		return connection;
-	}
-	
-	static Connection connectToDB() {
+	//Singleton Class
+	private DaoHelper() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(DB_URL,USER,PASS);
@@ -32,10 +27,14 @@ public class DaoHelper {
 	      //Handle errors for Class.forName
 	      e.printStackTrace();
 	   }
-		return connection;
-		
-		
-		   
 	}
+	
+	public static Connection getConnection() {
+		if(connection==null) {
+			new DaoHelper();
+		}
+		return connection;
+	}
+	
 
 }
