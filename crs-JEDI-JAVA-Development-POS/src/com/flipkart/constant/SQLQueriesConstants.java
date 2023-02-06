5449b3d2a81243ec4fb28221c25e5760d4a4e799
bt-7274-jack <ashwin.kumar2@flipkart.com>
2023-02-06 15:35:54 +0530

    Added More Funcationalities in StudentDAO SERVICE

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/constant/SQLQueriesConstants.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/constant/SQLQueriesConstants.java
@@ -19,30 +19,18 @@ public class SQLQueriesConstants {
 	
 	public static final String ADD_STUDENT_ID="INSERT INTO `crs_db`.`Student` (`student_id`, `email`, `approval_status`, `branch`, `semester`) VALUES ( ? , ? , 0 , ? , ?)";
 	
-	public static final String last_id="SELECT MAX(Student.student_id) FROM crs_db.Student";
+	public static final String LAST_ID="SELECT MAX(Student.student_id) FROM crs_db.Student";
+	
+	public static final String GET_COURSE_ID="SELECT RegisteredCourse.course_id FROM RegisteredCourse WHERE RegisteredCourse.student_id= ? AND RegisteredCourse.registration_status=2";
+	//public static final String GET_COURSE_NAME="SELECT Course.course_name in Course WHERE Course.course_id IN ?";
+	
+	public static final String GET_COURSES="SELECT * FROM Course WHERE Course.semester= ?";
+	
+	public static final String COUNT_CURRENT_COURSE_SEATS="SELECT COUNT(student_id) FROM RegisteredCourse WHERE course_id= ?";
+	
+	public static final String ADD_COURSE_IN_BUCKET="INSERT INTO `crs_db`.`RegisteredCourse` (`student_id`, `course_id`, `grade`, `registration_status`) VALUES (?, ?, \"GA\", ?)";
 	
 	
 	
-	
-	public static final String VIEW_REGISTERED_COURSES=" select * from course inner join registeredcourse on course.courseCode = registeredcourse.courseCode where registeredcourse.studentId = ?";
-	public static final String VIEW_AVAILABLE_COURSES=" select * from course where courseCode not in  (select courseCode  from registeredcourse where studentId = ?) and course.isOffered = ? and seats > 0";
-	public static final String CHECK_COURSE_AVAILABILITY=" select courseCode from registeredcourse where studentId = ? ";
-	public static final String DECREMENT_COURSE_SEATS="update course set seats = seats-1 where courseCode = ? ";
-	public static final String ADD_COURSE="insert into registeredcourse (studentId,courseCode) values ( ? , ? )";
-	public static final String DROP_COURSE_QUERY = "delete from registeredcourse where courseCode = ? AND studentId = ?;";
-	public static final String INCREMENT_SEAT_QUERY  = "update course set seats = seats + 1 where  courseCode = ?;";
-	public static final String CALCULATE_FEES  = "select sum(courseFee) from course where courseCode in (select courseCode from registeredcourse where studentId = ?);";
-	public static final String VIEW_GRADE = "select course.courseCode,course.courseName,registeredcourse.grade from course inner join registeredcourse on course.courseCode = registeredcourse.courseCode where registeredcourse.studentId = ?;";	
-	public static final String GET_SEATS = "select seats from course where courseCode = ?;";
-	public static final String INSERT_PAYMENT = "insert into payment(studentId,modeofPayment,referenceId,amount) values(?,?,?,?);";
-	public static final String INSERT_NOTIFICATION = "insert into notification(studentId,type,referenceId) values(?,?,?);";
-	public static final String GET_NOTIFICATION = "select * from notification where referenceId = ?;";
-	public static final String ADD_GRADE="update registeredcourse set Grade=? where courseCode=? and studentId=?";
-	public static final String GET_COURSES="select * from course where professorId=?";
-	public static final String GET_REGISTRATION_STATUS=" select isRegistered from student where studentId = ? ";
-	public static final String SET_REGISTRATION_STATUS="update student set isRegistered = true  where studentId=?";
-	public static final String GET_ENROLLED_STUDENTS="select course.courseCode,course.courseName,registeredcourse.studentId from course inner join registeredcourse on course.courseCode = registeredcourse.courseCode where course.professorId = ? order by course.courseCode";
-	public static final String NUMBER_OF_REGISTERED_COURSES=" select studentId from registeredcourse where studentId = ? ";
-	public static final String IS_REGISTERED=" select courseCode from registeredcourse where courseCode=? and studentId=? ";
 	
 }
