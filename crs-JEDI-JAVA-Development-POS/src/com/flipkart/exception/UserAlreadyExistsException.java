398d35dfe3cc961d709a53eceea5dccea4410424
MrRobot-29 <gaurav.dash@flipkart.com>
2023-02-07 10:29:33 +0530

    Local changes after hard reset

--- crs-JEDI-JAVA-Development-POS/src/com/flipkart/exception/UserAlreadyExistsException.java
+++ crs-JEDI-JAVA-Development-POS/src/com/flipkart/exception/UserAlreadyExistsException.java
@@ -1,5 +1,21 @@
 package com.flipkart.exception;
 
-public class UserAlreadyExistsException {
+public class UserAlreadyExistsException extends Exception{
+	private String userId;
 
+	/***
+	 * Setter function for UserId
+	 * @param userId
+	 */
+	public UserAlreadyExistsException(String id) {
+		userId = id;
+	}
+
+	/**
+	 * Message thrown by exception
+	 */
+	
+	public String getMessage() {
+		return "User with email: " + userId + " already present";
+	}
 }
