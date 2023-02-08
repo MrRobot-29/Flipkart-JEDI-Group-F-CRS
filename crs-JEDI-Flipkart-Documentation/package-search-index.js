2620c86d13746061c79b3cd77e868cd6fab9c3e4
bt-7274-jack <123713780+bt-7274-jack@users.noreply.github.com>
2023-02-08 11:49:39 +0530

    Delete crs-JEDI-Flipkart-Documentation directory

--- crs-JEDI-Flipkart-Documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.flipkart.bean"},{"l":"com.flipkart.client"},{"l":"com.flipkart.constant"},{"l":"com.flipkart.dao"},{"l":"com.flipkart.data"},{"l":"com.flipkart.exceptions"},{"l":"com.flipkart.helper"},{"l":"com.flipkart.service"},{"l":"com.flipkart.utils"},{"l":"com.flipkart.validation"}];updateSearchResults();
