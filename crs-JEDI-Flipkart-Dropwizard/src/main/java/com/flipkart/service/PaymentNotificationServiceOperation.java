7a481f52f3792bc1abb70e61d67e556ce6818afa
bt-7274-jack <ashwin.kumar2@flipkart.com>
2023-02-09 10:52:41 +0530

    Update PaymentService for the StudentRestApi controller

--- crs-JEDI-Flipkart-Dropwizard/src/main/java/com/flipkart/service/PaymentNotificationServiceOperation.java
+++ crs-JEDI-Flipkart-Dropwizard/src/main/java/com/flipkart/service/PaymentNotificationServiceOperation.java
@@ -7,19 +7,20 @@ import com.flipkart.dao.PaymentNotificationDaoImpl;
 public class PaymentNotificationServiceOperation implements PaymentNotificationService {
 
 	@Override
-	public void sendFeePaymentNotification(Student s, double billAmount) {
+	public String sendFeePaymentNotification(Student s, double billAmount) {
 		// TODO Auto-generated method stub
+
+		String result = "";
+
 		PaymentNotificationDaoImpl pndi = new PaymentNotificationDaoImpl();
 		PaymentNotification pn = pndi.sendFeePaymentNotification(s, billAmount);
 		System.out.println("Payment Successful");
 		System.out.println("Generating Payment info");
 		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
-		System.out.printf("%14s %14s %14s %21s %28s %14s", "Student Id","Student Name","Bill Amount","Mode of Payment","Payment Details","Payment Id");
+		System.out.printf("%14s %14s %14s %21s %28s %14s", "Student Id", "Student Name", "Bill Amount", "Mode of Payment", "Payment Details", "Payment Id");
 		System.out.println();
 		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
-		System.out.printf("%14s %14s %14s %21s %28s %14s", pn.getStudentId(),pn.getStudentName(),pn.getBillAmount(),pn.getModeOfPayment(),pn.getPaymentDetails(),pn.getPaymentId());
-		System.out.println();
-		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
+		return pn.getStudentId() + " " + pn.getStudentName() + " " +pn.getBillAmount() + " " +pn.getModeOfPayment() + " " +pn.getPaymentDetails() +" " + pn.getPaymentId();
 	}
 
 }
